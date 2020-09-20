fc37e5f253e8472e0a653fcae6891de55dfae3db
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2020-09-20 02:53:36 +0200

    [ Added ] demo project

---
+++ docs/JS/script.js
@@ -0,0 +1,24 @@
+function changePaddingByElementId(rangeInput, elementId) { 
+    let newPadding = rangeInput.value;
+
+    console.log(`${newPadding}px`);
+
+    document.getElementById(elementId).style.padding = `${newPadding}px`;
+}
+
+function changeMarginByElementId(rangeInput, elementId) { 
+    let newMargin = rangeInput.value;
+
+    console.log(`${newMargin}px`);
+
+    document.getElementById(elementId).style.margin = `${newMargin}px`;
+}
+
+
+function changeBorderWidthSideByElementId(rangeInput, elementId) { 
+    let newMargin = rangeInput.value;
+
+    console.log(`${newMargin}px`);
+
+    document.getElementById(elementId).style.borderWidth = `${newMargin}px`;
+}
